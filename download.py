6bd44f07920f3c416ef91c39bc048f514b6caf03
Simon Norris <snorris@hillcrestgeo.ca>
2024-08-23 11:09:31 -0700

    add prelim workflows from harvestrestrictions

--- download.py
+++ download.py
@@ -2,8 +2,12 @@ import json
 import logging
 import os
 from pathlib import Path
+import re
 import sys
+import zipfile
 
+import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 import click
 from cligj import verbose_opt, quiet_opt
 import geopandas
@@ -14,6 +18,32 @@ from slugify import slugify
 
 LOG_FORMAT = "%(asctime)s:%(levelname)s:%(name)s: %(message)s"
 LOG = logging.getLogger(__name__)
+
+
+def upload_file_to_s3(file_path, bucket_name, s3_key):
+    """
+    Upload a file to an S3 bucket.
+
+    :param file_path: Local path to the file to upload.
+    :param bucket_name: Name of the S3 bucket.
+    :param s3_key: Key (path) for the file in the S3 bucket.
+    :return: None
+    """
+    s3_client = boto3.client('s3')
+
+    try:
+        s3_client.upload_file(file_path, bucket_name, s3_key)
+        LOG.info(f"File {file_path} uploaded to {bucket_name}/{s3_key}")
+    except FileNotFoundError:
+        LOG.error(f"The file {file_path} was not found.")
+    except NoCredentialsError:
+        LOG.error("Credentials not available.")
+    except PartialCredentialsError:
+        LOG.error("Incomplete credentials provided.")
+    except ClientError as e:
+        LOG.error(f"Client error: {e}")
+    except Exception as e:
+        LOG.error(f"An error occurred: {e}")
 
 
 def configure_logging(verbosity):
@@ -59,7 +89,7 @@ def download_source(source):
     for column in source["fields"]:
         if column and column.lower() not in columns:
             raise ValueError(
-                f"Validation error: {source['alias']} - column {column} is not present, modify config 'field_mapper'"
+                f"Validation error: {source['alias']} - column {column} is not present, modify config 'fields'"
             )
 
     # is there data?
@@ -80,24 +110,55 @@ def download_source(source):
     return df
 
 
+def zip_gdb(folder_path, zip_path):
+    """
+    Compress the contents of an entire folder into a zip file.
+
+    :param folder_path: Path to the folder to be zipped.
+    :param zip_path: Path to the resulting zip file.
+    """
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        # Walk through the directory
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                # Create the full file path
+                file_path = os.path.join(root, file)
+                # Create a relative path for the file in the zip
+                relative_path = os.path.relpath(file_path, folder_path)
+                # Add file to the zip file
+                zipf.write(file_path, relative_path)
+
+
+def is_s3_path(path):
+    """
+    Check if the given path is an S3 path.
+
+    :param path: The path to check.
+    :return: True if the path is an S3 path, False otherwise.
+    """
+    # Define a regular expression to match S3 paths
+    s3_pattern = re.compile(r'^s3://')
+    return bool(s3_pattern.match(path))
+
+
 @click.command()
 @click.argument("sources_file", type=click.Path(exists=True), default="sources.json")
 @click.option(
     "--out_file",
-    "-of",
+    "-o",
     default="parks.gdb",
     help="Output file name",
 )
 @click.option(
-    "--out_path",
-    "-o",
+    "--out_prefix",
+    "-p",
     type=click.Path(),
     default=".",
-    help="Output path to write data (local or s3://)",
+    help="Output s3 prefix",
 )
 @click.option(
     "--out_layer",
-    "-of",
+    "-nln",
     default="parks",
     help="Name of output file",
 )
@@ -116,7 +177,7 @@ def download_source(source):
 @verbose_opt
 @quiet_opt
 def download(
-    sources_file, out_file, out_path, out_layer, source_alias, dry_run, verbose, quiet
+    sources_file, out_file, out_prefix, out_layer, source_alias, dry_run, verbose, quiet
 ):
     """Download sources defined in provided file"""
     configure_logging((verbose - quiet))
@@ -134,11 +195,12 @@ def download(
 
     # process all sources
     for source in sources:
+
         # download data, check to see if it meets expectations
         df = download_source(source)
 
         if not dry_run:
-            # clean
+
             # reproject to BC Albers if necessary
             if df.crs != CRS.from_user_input(3005):
                 df = df.to_crs("EPSG:3005")
@@ -150,16 +212,17 @@ def download(
             # retain only fields of interest
             df = df[[c.lower() for c in source["fields"]] + ["geom"]]
 
-            # dump to file
-            full_out_path = os.path.join(
-                out_path,
-                source["admin_area_group_name_abbreviation"],
-                source["alias"]
-            )
-            Path(full_out_path).mkdir(parents=True, exist_ok=True)
-            full_out_file = os.path.join(full_out_path, out_file)
-            df.to_file(full_out_file, driver="OpenFileGDB", layer=out_layer)
-            LOG.info(f"{source['alias']} written to {full_out_file}")
+            # dump to temp local file
+            df.to_file(out_file, driver="OpenFileGDB", layer=out_layer)
+
+            # compress
+            zip_gdb(out_file, out_file+".zip")
+
+            # upload
+            s3_key = "/".join([out_prefix, source[""], source[""], out_file + ".zip"])
+            upload_file_to_s3(out_file+".zip", os.environ.get("OBJECTSTORE_BUCKET"), s3_key)
+
+            LOG.info(f"{source['alias']} written to {s3_key}")
 
 
 if __name__ == "__main__":
