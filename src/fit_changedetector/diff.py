62d4dd72c1f40c1d66d2e107688535b7d02b0964
Simon Norris <snorris@hillcrestgeo.ca>
2024-09-11 16:55:55 -0700

    generate diff as five individual layers/geodataframes with required format

--- src/fit_changedetector/diff.py
+++ src/fit_changedetector/diff.py
@@ -2,8 +2,7 @@ import geopandas
 import fit_changedetector as fcd
 
 
-def load(source_file_a, source_file_b, primary_key, fields=None):
-    """Load input datasets and inspect - are they comparable?"""
+def differ(source_file_a, source_file_b, primary_key, fields=None, precision=2):
     df_a = geopandas.read_file(source_file_a)
     df_b = geopandas.read_file(source_file_b)
 
@@ -11,7 +10,14 @@ def load(source_file_a, source_file_b, primary_key, fields=None):
     fields_a = set([c for c in df_a.columns if c != "geometry"])
     fields_b = set([c for c in df_b.columns if c != "geometry"])
     fields_common = fields_a.intersection(fields_b)
+
+    # is primary key present in both datasets?
+    if primary_key not in fields_common:
+        raise ValueError(f"Primary key {primary_key} must be present in both datasets")
+
+    # if provided a list of fields to work with, validate that list
     if fields:
+        fields = list(set(fields + [primary_key]))
         if len(set(fields).intersection(fields_common)) != len(fields):
             raise ValueError("Provided fields are not common to both datasets")
     else:
@@ -20,11 +26,7 @@ def load(source_file_a, source_file_b, primary_key, fields=None):
     if len(fields) == 0:
         raise ValueError("Datasets have no field names in common, cannot compare")
 
-    # check primary key is present in both datasets (it must have the same name)
-    if primary_key not in fields:
-        raise ValueError(f"Primary key {primary_key} must be present in both datasets")
-
-    # only retain data for common fields of interest (and geometry)
+    # remove all columns other than primary key, common fields of interest, and geometry
     df_a = df_a[fields + ["geometry"]]
     df_b = df_b[fields + ["geometry"]]
 
@@ -65,12 +67,6 @@ def load(source_file_a, source_file_b, primary_key, fields=None):
     df_a = df_a.set_index(primary_key)
     df_b = df_b.set_index(primary_key)
 
-    # if no error raised, return both dataframes
-    return (df_a, df_b)
-
-
-def diff(df_a, df_b, tolerance):
-    """compare data frames by joining on indexes (primary keys)"""
     # find additions / deletions by joining on indexes
     joined = df_a.merge(
         df_b,
@@ -84,33 +80,72 @@ def diff(df_a, df_b, tolerance):
     deletions = joined[joined["_merge"] == "left_only"]
     common = joined[joined["_merge"] == "both"]
 
-    # find rows with modified attributes
+    # clean column names in resulting dataframes
     columns = list(df_a.columns)
-    # remove the a/b suffixes from the join result,
-    # generating two new df with rows common to inputs
     column_name_remap_a = {k + "_a": k for k in columns}
     column_name_remap_b = {k + "_b": k for k in columns}
+    # additions is data from source b
+    additions = additions.rename(columns=column_name_remap_b)[columns]
+    # deletions is data from source a
+    deletions = deletions.rename(columns=column_name_remap_a)[columns]
+
+    # create two dataframes holding records from respective source
+    # that are common to both sources
     common_a = common.rename(columns=column_name_remap_a)[columns]
     common_b = common.rename(columns=column_name_remap_b)[columns]
 
-    # compare the attributes, returning only those that have changed
+    # compare the attributes
     common_a_attrib = common_a.drop("geometry", axis=1)
     common_b_attrib = common_b.drop("geometry", axis=1)
-    modifications_attributes = common_a_attrib.compare(
-        common_b_attrib, result_names=("a", "b")
+    modified_attributes = common_a_attrib.compare(
+        common_b_attrib,
+        result_names=(
+            "a",
+            "b",
+        ),
+        keep_shape=True,
+    ).dropna(axis=0, how="all")
+
+    # flatten the resulting data structure
+    modified_attributes.columns = [
+        "_".join(a) for a in modified_attributes.columns.to_flat_index()
+    ]
+
+    # join back to geometries in b, creating attribute diff
+    modified_attributes.merge(
+        common_b["geometry"], how="inner", left_index=True, right_index=True
+    )
+    # note the columns generated
+    attribute_diff_columns = list(modified_attributes.columns.values) + ["geometry"]
+
+    # find all rows with modified geometries, retaining new geometries only
+    common_mod_geoms = common.rename(columns=column_name_remap_b)[columns]
+    modified_geometries = common_mod_geoms[
+        ~common_a.geom_equals_exact(common_b, precision)
+    ]
+
+    # join modified attributes to modified geometries
+    modified_attributes_geometries = modified_attributes.merge(
+        modified_geometries,
+        how="outer",
+        left_index=True,
+        right_index=True,
+        indicator=True,
     )
 
-    # find rows with modified geometries
-    modifications_geometries = common[~common_a.geom_almost_equals(common_b, tolerance)]
+    # two sets of modified attributes, using diff columns
+    m_attributes = modified_attributes_geometries[
+        modified_attributes_geometries["_merge"] == "left_only"
+    ][attribute_diff_columns]
+    m_attributes_geometries = modified_attributes_geometries[
+        modified_attributes_geometries["_merge"] == "both"
+    ][attribute_diff_columns]
 
-    # find rows with modified geometries AND attributes
-    modifications_both = set(list(modifications_attributes.index.array)) & set(
-        list(modifications_geometries.index.array)
-    )
-    return (
-        additions,
-        deletions,
-        modifications_attributes,
-        modifications_geometries,
-        modifications_both,
-    )
+    # modified geoms only, using source column names
+    m_geometries = modified_attributes_geometries[
+        modified_attributes_geometries["_merge"] == "right_only"
+    ][columns]
+
+    # todo - returning 5 dataframes is fine,
+    # but as a dict prob better? or as properties of a diff/change detector object?
+    return (additions, deletions, m_attributes_geometries, m_attributes, m_geometries)
